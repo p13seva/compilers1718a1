d76f341c9ddb34077d4557376f31925be3d6ae3c
p13seva <p13seva@ionio.gr>
2018-03-21 13:24:17 +0200

    Update scanner.py

--- scanner.py
+++ scanner.py
@@ -1,79 +1,71 @@
+"""
+Sample script to test ad-hoc scanning by table drive.
+This accepts term,test and long
+NOTE: not suitable for longest/optional matches
+"""
 
 def getchar(words,pos):
 	""" returns char at pos of words, or None if out of bounds """
 
 	if pos<0 or pos>=len(words): return None
-
 	return words[pos]
 	
 
 def scan(text,transition_table,accept_states):
-	""" Scans `text` while transitions exist in 'transition_table'.
-	After that, if in a state belonging to `accept_states`,
-	returns the corresponding token, else ERROR_TOKEN.
+	""" scans `text` while transitions exist in
+	'transition_table'. After that, if in a state belonging to
+	`accept_states`, it returns the corresponding token, else ERROR.
 	"""
 	
 	# initial state
 	pos = 0
-	state = 'q0'
+	state = 's0'
 	
 	while True:
 		
 		c = getchar(text,pos)	# get next char
 		
-		if state in transition_table and c in transition_table[state]:
-		
+		if c in transition_table[state]:
 			state = transition_table[state][c]	# set new state
 			pos += 1	# advance to next char
 			
-		else:	# no transition found
-
-			# check if current state is accepting
+			# check if new state is accepting
 			if state in accept_states:
 				return accept_states[state],pos
 
-			# current state is not accepting
-			return 'ERROR_TOKEN',pos
+			
+		else:	# no transition found
+			return 'ERROR',pos
 			
 	
 # the transition table, as a dictionary
-
-# Αντικαταστήστε με το δικό σας λεξικό μεταβάσεων...
-td = { 'q0':{ 't':'q1','l':'q2' },
-       'q1':{ 'e':'q3' },
-       'q2':{ 'o':'q8' },
-       'q3':{ 's':'q4','r':'q6' },
-       'q4':{ 't':'q5' },
-       'q6':{ 'm':'q7' },
-       'q8':{ 'n':'q9' },
-       'q9':{ 'g':'q10'}
+td = { 's0':{ '0':'s1','1':'s1','2': 's2','3': 's3','4': 's3','5': 's3','6': 's3','7': 's3','8': 's3','9':'s3' },
+       's1':{ '0':'s4','1':'s4','2':'s4','3':'s4','4':'s4','5':'s4','6':'s4','7':'s4','8':'s4','9':'s4' },
+       's2':{ '0':'s6','1':'s6','2':'s6','3':'s6' },
+       's3':{ ':': 's5','.': 's5'},
+       's4':{ ':': 's5','.': 's5' },
+       's5':{ '0':'s7','1':'s7','2':'s7','3':'s7','4':'s7','5':'s7' },
+       's6':{ ':': 's5','.': 's5' },
+       's7':{ '0':'s8f','1':'s8f','2':'s8f','3':'s8f','4':'s8f','5':'s8f','6':'s8f','7':'s8f','8':'s8f','9':'s8f' },
      } 
 
 # the dictionary of accepting states and their
 # corresponding token
-
-# Αντικαταστήστε με το δικό σας λεξικό καταστάσεων αποδοχής...
-ad = { 'q5':'TEST_TOKEN',
-       'q7':'TERM_TOKEN',
-       'q10':'LONG_TOKEN'
+ad = { 's8f':'TIME_TOKEN'
      }
 
 
 # get a string from input
-text = input('give some input>')
+words = input('give some input>')
 
 # scan text until no more input
-while text:	# that is, while len(text)>0
-	
+while len(words)>0:
 	# get next token and position after last char recognized
-	token,position = scan(text,td,ad)
-	
-	if token=='ERROR_TOKEN':
-		print('unrecognized input at pos',position+1,'of',text)
+	tok,pos = scan(words,td,ad)
+	if tok=='ERROR':
+		print('unrecognized input at pos',pos,'of',words)
 		break
-	
-	print("token:",token,"string:",text[:position])
-	
-	# remaining text for next scan
-	text = text[position:]
-	
+	print("token:",tok,"text:",words[:pos])
+	# new text for next scan
+	words = words[pos:]
+
